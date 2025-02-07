4464560999e94f078c43c39d265a836eb0df3327
Guilherme Bezerra <gbsantos.it@gmail.com>
2025-02-06 23:58:53 -0300

    📝 update: creating GET, POST, PUT and DELETE endpoints

---
+++ productClientHub.API/Controllers/ClientsController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace productClientHub.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientsController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+            return Ok();
+        }
+
+        [HttpPut]
+        public IActionResult Update()
+        {
+            return Ok();
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok();
+        }
+
+        //[HttpGet("by-id")]
+        [Route("{id}")]
+        public IActionResult GetById(Guid id)
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+    }
+}
